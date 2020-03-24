fdec62409d9b230b158be3862532c8631e1cd16a
anonymous-one <riddlariddla@hotmail.com>
2020-03-24 23:06:53 +0100

    Update wifievent.sh

--- wifievent.sh
+++ wifievent.sh
@@ -3,3 +3,7 @@
 if [ $2 = "BEACON-RESP-RX" ]; then
         echo "$6" > "/tmp/beaconresp.$3"
 fi
+
+if [ $2 = "BEACON-REQ-TX-STATUS" ] && [ "$5" = "ack=0" ]; then
+        echo "FAILED" > "/tmp/beaconresp.$3"
+fi
