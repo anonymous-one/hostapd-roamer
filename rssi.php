190df0757924d34236d14dd9f93a2ba5e2cc1aca
anonymous-one <riddlariddla@hotmail.com>
2020-03-28 12:37:56 +0100

    Update rssi.php

--- rssi.php
+++ rssi.php
@@ -2,14 +2,14 @@
 function logToConsole($string){ // simple console logger
         echo date('Y-m-d H:i:s',time()).' | '.$string."\n";
 }
-function ascii2hex($ascii){
-        $hex='';
+function ascii2hex($ascii){ // convert ascii to hex
+        $hex=array();
         for($i=0;$i<strlen($ascii);$i++){
-                $byte=strtoupper(dechex(ord($ascii{$i})));
+                $byte=strtoupper(dechex(ord(substr($ascii,$i,1))));
                 $byte=str_repeat('0',2-strlen($byte)).$byte;
-                $hex.=$byte;
+                $hex[]=$byte;
         }
-        return $hex;
+        return implode($hex);
 }
 function getDeviceRSSI($mac){ // fetch station details using iw, check all adapters, and use the freshest one
         global $data;
@@ -52,8 +52,10 @@ function fetchRssi($params,$timeout=5){ // send out a beacon request, and wait f
         $parts[]=str_pad(dechex($target['chan']),2,'0',STR_PAD_LEFT);
         // randomization interval *** not sure what this is used for, sounds like how long to wait before starting a scan ***
         $parts[]='0000';
-        // duration *** still have not figured this out fully, but this value works well for me ***
-        $parts[]='3000';
+        // duration *** still have not figured this out fully, but i am using this to identify the beacon request ***
+        $id=rand(25,35);
+        $rand=str_pad(dechex($id),2,'0',STR_PAD_LEFT).'00';
+        $parts[]=$rand;
         // mode [ 0 = passive, 1 = active, 2 = table ]
         $parts[]='01';
         // target mac
@@ -79,6 +81,15 @@ function fetchRssi($params,$timeout=5){ // send out a beacon request, and wait f
                         }
                         elseif(strlen($content)<64){ // 64 is just a random length, but these beacon responses are quite long, so 64 is enough
                                 return false;
+                        }
+                        // parse out the returned mac address
+                        $mac=substr($content,30,12);
+                        if($mac!=str_replace(':','',$params['targetmac'])){
+                                return NULL;
+                        }
+                        // parse out the id of the request
+                        if(hexdec(substr($content,20,2))!=$id){
+                                return NULL;
                         }
                         $rssi=unpack("l", pack("l", hexdec("FFFFFF".strtoupper(substr($content,26,2)))))[1]; // pull out the rssi from the beacon response
                         // the rssi is returned in a -XXdBm format, convert to positive to make more readable
@@ -108,50 +119,6 @@ function neighborFromMac($mac,$returnparsed=true){ // parse out various details 
                 $final['phytype']=$phy;
         }
         return $final;
-}
-function sortBeaconCheck($freq,$candidates){
-        /*
-        sort the beaconcheck list according to the passed candidate rssi list:
-
-        for beaconcheck lists that have 2 or more entries this will make the final check a bit quicker
-        the entry with the highest rssi will be at the top of the list and the script will skip all other
-        entries if it meets the required minimum rssi
-
-        think of it this way, as you are walking towards a potential roam-to access point
-        your signal will get stronger and stronger until the rssi is strong enough for a roam-to
-        its this entry we want to have at the top of the beaconcheck list so when it meets the
-        minimum rssi, everything else is skipped and we send over a bss-tm
-
-        eg:
-        2nd to last cycle : AP2 - 70, AP3 - 66, AP1 - 72
-        last cycle : AP3 - 64 (enough for a roam), does not check other entries
-        */
-        global $beaconcheck;
-        // cannot sort a list with less than 2 entries
-        if(count($beaconcheck[$freq])<2){
-                return false;
-        }
-        // sort the candidate list according to rssi
-        asort($candidates);
-        $final=array();
-        foreach($candidates as $candidate=>$rssi){
-                $final[]=$candidate;
-        }
-        // add all entries that are not already in the final incase the returnd a false (not in range) or are our own mac address
-        foreach($beaconcheck[$freq] as $mac){
-                if(in_array($mac,$final)){
-                        continue;
-                }
-                $final[]=$mac;
-        }
-        // if the order of the list has changed, send out a logToConsole
-        if(serialize($final)!=serialize($beaconcheck[$freq])){
-                logToConsole('Beaconcheck frequency '.$freq.' order has been modified');
-                return $final;
-        }
-        else{
-                return false;
-        }
 }
 
 // include the config file
@@ -219,6 +186,11 @@ while(true){
         // i only have 1 roamer i need to worry about, but this lays the foundation for making this script handle more than one
         foreach($roamers as $mac=>$devicedata){
                 if(!$rssidata=getDeviceRSSI($mac)){ // fetch rssi via iw, if not connected, wait 50ms and try again
+                        $roamers[$mac]['failcount']=0;
+                        usleep(50000);
+                        continue;
+                }
+                elseif($rssidata['connectedtime']<1){ // wait until client has been connected for at least 1 second
                         usleep(50000);
                         continue;
                 }
@@ -230,77 +202,54 @@ while(true){
                 $params['targetnr']=$neigh[$rssidata['apmac']];
                 $result=fetchRssi($params,1);
                 if($result===NULL||$result===false){
-                        usleep(100000);
-                        continue;
+                        $roamers[$mac]['failcount']=(isset($roamers[$mac]['failcount'])?$roamers[$mac]['failcount']+1:1);
                 }
                 else{
+                        $roamers[$mac]['failcount']=0;
                         $rssidata['rssi']=$result;
                 }
-                if($rssidata['freq']==5&&$rssidata['rssi']>=$data['rssi-5-to-other']){ // see if we should roam from AP1 5ghz to AP2 5ghz
+                if($roamers[$mac]['failcount']>=10){
+                        $command='hostapd_cli -i '.$rssidata['apdevice'].' deauthenticate '.$mac.' 2>&1';
+                        logToConsole('Deauthenticating '.$mac.' via '.$command.' ['.trim(shell_exec($command)).']');
+                        sleep(1);
+                        $roamers[$mac]['failcount']=0;
+                        continue;
+                }
+                elseif(in_array($result,array(NULL,false))){
+                        usleep(50000);
+                        continue;
+                }
+                if(isset($data['adapters']['5'])&&$rssidata['freq']==5&&$rssidata['rssi']>=$data['rssi-5-to-other']){ // connected at 5 + rssi above rssi-5-to-other = try to roam to $beaconcheck[5]
                         $candidates=array();
-                        foreach($beaconcheck[5] as $key=>$target){ // send out a beacon request to each of the APs we could possibly roam to
-                                if(!isset($beaconcache[$target])||microtime(true)-$beaconcache[$target][0]>0.1){ // just in case, lets cache this data for 100ms
-                                        $params=array();
-                                        $params['stamac']=$mac;
-                                        $params['staadapter']=$data['adapters'][5][0];
-                                        $params['targetmac']=$target;
-                                        $params['targetnr']=$neigh[$target];
-                                        $result=fetchRssi($params);
-                                        if($result===false){
-                                                $beaconcache[$target]=array((microtime(true)+0.2),$result);
-                                        }
-                                        else{
-                                                $beaconcache[$target]=array(microtime(true),$result);
-                                        }
-                                }
-                                else{
-                                        $result=$beaconcache[$target][1];
-                                }
-                                if($result===false){ // no beacon response? continue to the next ap
-                                        continue;
-                                }
-                                elseif($target==$data['adapters'][5][1]){ // are we checking the current connected to ap? replace the rssi
-                                        $rssidata['rssi']=$result;
-                                }
-                                elseif($result<=$data['rssi-5-to-other-min']){ // does the rssi to the candidate meet the minimum required rssi? if so, store as a possible target
+                        foreach($beaconcheck[5] as $key=>$target){
+                                $params=array();
+                                $params['stamac']=$mac;
+                                $params['staadapter']=$data['adapters'][5][0];
+                                $params['targetmac']=$target;
+                                $params['targetnr']=$neigh[$target];
+                                $result=fetchRssi($params);
+                                if($result!==NULL&&$result!==false&&$result<=$data['rssi-5-to-other-min']){
                                         $candidates[$target]=$result;
-                                        if($result<$rssidata['rssi']){ // maybe we do not need to check any other access points
-                                                break;
-                                        }
                                 }
                         }
-                        foreach($candidates as $candidatemac=>$candidaterssi){ // lets check each of the possible candidates
-                                if($candidatemac==$rssidata['apmac']){ // candidate is the same ap as already connected to? continue onto next result
-                                        continue;
-                                }
-                                elseif($candidaterssi<$rssidata['rssi']){ // is the rssi to the candidate lower than the currently connected to ap? force a roam.
-                                        $command='hostapd_cli -i '.$data['adapters'][5][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
-                                        $output=array();
-                                        exec($command,$output);
-                                        logToConsole('Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(implode(' ',$output)).']');
-                                        usleep(500000);
-                                        continue 2;
-                                }
-                        }
-                        // see if we can re-sort the beaconcheck list
-                        if($sorted=sortBeaconCheck(5,$candidates)){
-                                $beaconcheck[5]=$sorted;
+                        asort($candidates);
+                        foreach($candidates as $candidatemac=>$candidaterssi){
+                                $command='hostapd_cli -i '.$data['adapters'][5][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
+                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
+                                usleep(500000);
+                                continue 2;
                         }
                 }
-                elseif(isset($data['adapters'][5])&&$rssidata['freq']==2&&$rssidata['rssi']<=$data['rssi-2-to-5']&&$rssidata['connectedtime']>=2){ // are we connected to the 2.4ghz band + have a strong signal? try to roam to 5ghz band on the same AP
-                        // send out a beacon report request to the 5ghz band ap
+                elseif(isset($data['adapters']['2'])&&$rssidata['freq']==2&&$rssidata['rssi']<=$data['rssi-2-to-5']){ // connected at 2 + rssi below rssi-2-to-5 = try to roam to same ap 5
                         $params=array();
                         $params['stamac']=$mac;
                         $params['staadapter']=$data['adapters'][2][0];
                         $params['targetmac']=$data['adapters'][5][1];
                         $params['targetnr']=$neigh[$data['adapters'][5][1]];
                         $result=fetchRssi($params);
-                        if($result!==false&&$result<=65){ // do we have a response and the rssi is good? force a roam.
+                        if($result!==NULL&&$result!==false&&$result<=$data['rssi-2-to-5-min']){
                                 $command='hostapd_cli -i '.$data['adapters'][2][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($data['adapters'][5][1]).' pref=1 abridged=1 2>&1';
-                                $output=array();
-                                exec($command,$output);
-                                logToConsole('Forced '.$mac.' to roam to 5ghz via '.$command.' ['.trim(implode(' ',$output)).']');
-                                // for 3 seconds check if the client changed frequencies
+                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] 5ghz band has rssi of '.$result.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
                                 $start=time();
                                 while(time()-$start<=3){
                                         $roamdata=getDeviceRSSI($mac);
@@ -318,55 +267,25 @@ while(true){
                                 continue;
                         }
                 }
-                elseif($rssidata['freq']==2&&$rssidata['rssi']>=70&&$rssidata['connectedtime']>=2){ // are we connected to the 2.4ghz band + have a fairly weak signal? try to roam to either a 5ghz AP or 2.4ghz AP
+                elseif(isset($data['adapters']['2'])&&$rssidata['freq']==2&&$rssidata['rssi']>=$data['rssi-2-to-other']){ // connected at 2 + rssi above rssi-2-to-other = try to roam to $beaconcheck[2]
                         $candidates=array();
-                        foreach($beaconcheck[2] as $key=>$target){ // send out a beacon request to each of the APs we could possibly roam to
-                                if(!isset($beaconcache[$target])||microtime(true)-$beaconcache[$target][0]>0.1){ // just in case, lets cache this data for 100ms
-                                        $params=array();
-                                        $params['stamac']=$mac;
-                                        $params['staadapter']=$data['adapters'][2][0];
-                                        $params['targetmac']=$target;
-                                        $params['targetnr']=$neigh[$target];
-                                        $result=fetchRssi($params);
-                                        if($result===false){
-                                                $beaconcache[$target]=array((microtime(true)+0.2),$result);
-                                        }
-                                        else{
-                                                $beaconcache[$target]=array(microtime(true),$result);
-                                        }
-                                }
-                                else{
-                                        $result=$beaconcache[$target][1];
-                                }
-                                if($result===false){ // no beacon response? continue to the next ap
-                                        continue;
-                                }
-                                elseif($target==$data['adapters'][2][1]){ // are we checking the current connected to ap? replace the rssi
-                                        $rssidata['rssi']=$result;
-                                }
-                                elseif($result<=$data['rssi-5-to-other-min']){ // does the rssi to the candidate meet the minimum required rssi? if so, store as a possible target
+                        foreach($beaconcheck[2] as $key=>$target){
+                                $params=array();
+                                $params['stamac']=$mac;
+                                $params['staadapter']=$data['adapters'][2][0];
+                                $params['targetmac']=$target;
+                                $params['targetnr']=$neigh[$target];
+                                $result=fetchRssi($params);
+                                if($result!==NULL&&$result!==false&&$result<=$data['rssi-2-to-other-min']){
                                         $candidates[$target]=$result;
-                                        if($result<($rssidata['rssi']-5)){ // maybe we do not need to check any other access points
-                                                break;
-                                        }
                                 }
                         }
-                        foreach($candidates as $candidatemac=>$candidaterssi){ // lets check each of the possible candidates
-                                if($candidatemac==$rssidata['apmac']){ // candidate is the same ap as already connected to? continue onto next result
-                                        continue;
-                                }
-                                elseif($candidaterssi<($rssidata['rssi']-5)){ // is the rssi to the candidate lower than the currently connected to ap? force a roam.
-                                        $command='hostapd_cli -i '.$data['adapters'][2][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
-                                        $output=array();
-                                        exec($command,$output);
-                                        logToConsole('Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(implode(' ',$output)).']');
-                                        usleep(500000);
-                                        continue 2;
-                                }
-                        }
-                        // see if we can re-sort the beaconcheck list
-                        if($sorted=sortBeaconCheck(2,$candidates)){
-                                $beaconcheck[2]=$sorted;
+                        asort($candidates);
+                        foreach($candidates as $candidatemac=>$candidaterssi){
+                                $command='hostapd_cli -i '.$data['adapters'][2][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
+                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
+                                usleep(500000);
+                                continue 2;
                         }
                 }
         }
