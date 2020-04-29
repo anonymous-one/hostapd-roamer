9cdd0d29515bdde31a0ea491df9e48ca4d3d5d57
anonymous-one <riddlariddla@hotmail.com>
2020-04-29 20:27:19 +0200

    Update rssi.php
    Stopped using beacon request duration to identify beacon requests as this was causing issues.
    
    Added failed beacon tracking (ack=0) to automatically deauthenticate clients after 10 failed beacon requests.
    
    Quite a bit of cleanup.

--- rssi.php
+++ rssi.php
@@ -52,10 +52,8 @@ function fetchRssi($params,$timeout=5){ // send out a beacon request, and wait f
         $parts[]=str_pad(dechex($target['chan']),2,'0',STR_PAD_LEFT);
         // randomization interval *** not sure what this is used for, sounds like how long to wait before starting a scan ***
         $parts[]='0000';
-        // duration *** still have not figured this out fully, but i am using this to identify the beacon request ***
-        $id=rand(25,35);
-        $rand=str_pad(dechex($id),2,'0',STR_PAD_LEFT).'00';
-        $parts[]=$rand;
+        // duration [ 100 dec = 64 hex ]
+        $parts[]='0064';
         // mode [ 0 = passive, 1 = active, 2 = table ]
         $parts[]='01';
         // target mac
@@ -85,10 +83,6 @@ function fetchRssi($params,$timeout=5){ // send out a beacon request, and wait f
                         // parse out the returned mac address
                         $mac=substr($content,30,12);
                         if($mac!=str_replace(':','',$params['targetmac'])){
-                                return NULL;
-                        }
-                        // parse out the id of the request
-                        if(hexdec(substr($content,20,2))!=$id){
                                 return NULL;
                         }
                         $rssi=unpack("l", pack("l", hexdec("FFFFFF".strtoupper(substr($content,26,2)))))[1]; // pull out the rssi from the beacon response
@@ -186,11 +180,12 @@ while(true){
         // i only have 1 roamer i need to worry about, but this lays the foundation for making this script handle more than one
         foreach($roamers as $mac=>$devicedata){
                 if(!$rssidata=getDeviceRSSI($mac)){ // fetch rssi via iw, if not connected, wait 50ms and try again
-                        $roamers[$mac]['failcount']=0;
+                        $roamers[$mac]['failedbeacons']=0;
                         usleep(50000);
                         continue;
                 }
                 elseif($rssidata['connectedtime']<1){ // wait until client has been connected for at least 1 second
+                        $roamers[$mac]['failedbeacons']=0;
                         usleep(50000);
                         continue;
                 }
@@ -202,23 +197,23 @@ while(true){
                 $params['targetnr']=$neigh[$rssidata['apmac']];
                 $result=fetchRssi($params,1);
                 if($result===NULL||$result===false){
-                        $roamers[$mac]['failcount']=(isset($roamers[$mac]['failcount'])?$roamers[$mac]['failcount']+1:1);
-                }
-                else{
-                        $roamers[$mac]['failcount']=0;
-                        $rssidata['rssi']=$result;
-                }
-                if($roamers[$mac]['failcount']>=10){
-                        $command='hostapd_cli -i '.$rssidata['apdevice'].' deauthenticate '.$mac.' 2>&1';
-                        logToConsole('Deauthenticating '.$mac.' via '.$command.' ['.trim(shell_exec($command)).']');
-                        sleep(1);
-                        $roamers[$mac]['failcount']=0;
-                        continue;
-                }
-                elseif(in_array($result,array(NULL,false))){
+                        // track and handle failed beacons
+                        if($result===NULL){
+                                $roamers[$mac]['failedbeacons']++;
+                                if($roamers[$mac]['failedbeacons']>=10){
+                                        // deauth this client as its not responding to beacon requests ( ack=0 )
+                                        $command='hostapd_cli -i '.$rssidata['apdevice'].' deauthenticate '.$mac;
+                                        logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Deauthenticating '.$mac.' via '.$command);
+                                        system($command);
+                                        $roamers[$mac]['failedbeacons']=0;
+                                        continue;
+                                }
+                        }
                         usleep(50000);
                         continue;
                 }
+                $roamers[$mac]['failedbeacons']=0;
+                $rssidata['rssi']=$result;
                 if(isset($data['adapters']['5'])&&$rssidata['freq']==5&&$rssidata['rssi']>=$data['rssi-5-to-other']){ // connected at 5 + rssi above rssi-5-to-other = try to roam to $beaconcheck[5]
                         $candidates=array();
                         foreach($beaconcheck[5] as $key=>$target){
@@ -229,16 +224,15 @@ while(true){
                                 $params['targetnr']=$neigh[$target];
                                 $result=fetchRssi($params);
                                 if($result!==NULL&&$result!==false&&$result<=$data['rssi-5-to-other-min']){
-                                        $candidates[$target]=$result;
+                                        $command='hostapd_cli -i '.$data['adapters'][5][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($target).' pref=1 abridged=1 2>&1';
+                                        logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$target.' has rssi of '.$result.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
+                                        sleep(2);
+                                        continue 2;
                                 }
+                                $candidates[$target]=($result?$result:81);
                         }
                         asort($candidates);
-                        foreach($candidates as $candidatemac=>$candidaterssi){
-                                $command='hostapd_cli -i '.$data['adapters'][5][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
-                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
-                                usleep(500000);
-                                continue 2;
-                        }
+                        $beaconcheck[5]=array_keys($candidates);
                 }
                 elseif(isset($data['adapters']['2'])&&$rssidata['freq']==2&&$rssidata['rssi']<=$data['rssi-2-to-5']){ // connected at 2 + rssi below rssi-2-to-5 = try to roam to same ap 5
                         $params=array();
@@ -258,13 +252,11 @@ while(true){
                                                 continue;
                                         }
                                         elseif($roamdata['freq']==5){
-                                                logToConsole('Success roaming to 5ghz!');
-                                                continue 2;
+                                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Success roaming to 5ghz!');
+                                                break;
                                         }
                                         usleep(50000);
                                 }
-                                logToConsole('Failed roaming to 5ghz!');
-                                continue;
                         }
                 }
                 elseif(isset($data['adapters']['2'])&&$rssidata['freq']==2&&$rssidata['rssi']>=$data['rssi-2-to-other']){ // connected at 2 + rssi above rssi-2-to-other = try to roam to $beaconcheck[2]
@@ -277,16 +269,14 @@ while(true){
                                 $params['targetnr']=$neigh[$target];
                                 $result=fetchRssi($params);
                                 if($result!==NULL&&$result!==false&&$result<=$data['rssi-2-to-other-min']){
-                                        $candidates[$target]=$result;
+                                        $command='hostapd_cli -i '.$data['adapters'][2][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($target).' pref=1 abridged=1 2>&1';
+                                        logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$target.' has rssi of '.$result.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
+                                        break;
                                 }
+                                $candidates[$target]=($result?$result:81);
                         }
                         asort($candidates);
-                        foreach($candidates as $candidatemac=>$candidaterssi){
-                                $command='hostapd_cli -i '.$data['adapters'][2][0].' bss_tm_req '.$mac.' neighbor='.neighborFromMac($candidatemac).' pref=1 abridged=1 2>&1';
-                                logToConsole('['.$mac.' @ '.$rssidata['freq'].':'.$rssidata['rssi'].'] Candidate '.$candidatemac.' has rssi of '.$candidaterssi.', forced roam via '.$command.' ['.trim(shell_exec($command)).']');
-                                usleep(500000);
-                                continue 2;
-                        }
+                        $beaconcheck[2]=array_keys($candidates);
                 }
         }
         usleep(500000);
